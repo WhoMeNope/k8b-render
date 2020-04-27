bf28cf1a8ad210be0225b046c6eeb9d1edd85874
Martin Toman <whomenope@outlook.com>
2020-04-27 23:01:07 +0200

    Lorri + niv + nix-shell

---
+++ shell.nix
@@ -0,0 +1,17 @@
+{ sources ? import ./nix/sources.nix }:
+with { overlay = _: pkgs: {
+  niv = import sources.niv { };
+}; };
+
+with import sources.nixpkgs {
+  overlays = [ overlay ];
+  config = { };
+};
+
+mkShell {
+  buildInputs = [
+    go
+    gotools
+    gocode
+  ];
+}
