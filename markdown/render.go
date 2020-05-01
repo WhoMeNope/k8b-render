fc9d12f0b56c225af5cc8737778b78b2baea546e
Martin Toman <whomenope@outlook.com>
2020-05-01 19:36:38 +0200

    Allow class attribute, add highlighting stylerules

--- markdown/render.go
+++ markdown/render.go
@@ -2,6 +2,8 @@ package render
 
 import (
 	"bytes"
+	"fmt"
+
 	"github.com/alecthomas/chroma/formatters/html"
 	"github.com/microcosm-cc/bluemonday"
 	"github.com/yuin/goldmark"
@@ -11,13 +13,18 @@ import (
 )
 
 func Render(source []byte) ([]byte, error) {
+	var css bytes.Buffer
+
 	md := goldmark.New(
 		goldmark.WithExtensions(
 			extension.GFM,
 			extension.Footnote,
 			highlighting.NewHighlighting(
 				highlighting.WithStyle("murphy"),
+				highlighting.WithCSSWriter(&css),
 				highlighting.WithFormatOptions(
+					// use classes rather than inline styles
+					html.WithClasses(true),
 					// copy-friendly line numbers
 					html.WithLineNumbers(true),
 					html.LineNumbersInTable(true),
@@ -40,6 +47,13 @@ func Render(source []byte) ([]byte, error) {
 	// Do this once for each unique policy, and use the policy for the life of the program
 	// Policy creation/editing is not safe to use in multiple goroutines
 	p := bluemonday.UGCPolicy()
+	p.AllowAttrs("class").Globally()
 
-	return p.SanitizeBytes(buf.Bytes()), nil
+	var output bytes.Buffer
+	_, err := fmt.Fprintf(&output, "<style>\n%s</style>\n%s", css.Bytes(), p.SanitizeBytes(buf.Bytes()))
+	if err != nil {
+		return nil, err
+	}
+
+	return output.Bytes(), nil
 }
