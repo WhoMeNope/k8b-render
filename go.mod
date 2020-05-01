c2f5f17be85c490a7c82542ccdeb2dd1edff7870
Martin Toman <whomenope@outlook.com>
2020-05-01 16:23:18 +0200

    Switch snapshot testing (snapshot->cupaloy)

--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module github.com/WhoMeNope/k8b-render
 go 1.14
 
 require (
-	github.com/WhoMeNope/snapshot v1.1.0
+	github.com/bradleyjkemp/cupaloy v2.3.0+incompatible
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/yuin/goldmark v1.1.30
 )
--- markdown/render_test.go
+++ markdown/render_test.go
@@ -3,7 +3,7 @@ package render
 import (
 	"testing"
 
-	"github.com/WhoMeNope/snapshot"
+	"github.com/bradleyjkemp/cupaloy"
 )
 
 const multiline = `
@@ -20,19 +20,13 @@ var testCases = map[string]string{
 func TestRender(t *testing.T) {
 	// iterate over all tests
 	for name, input := range testCases {
+		t.Run(name, func(t *testing.T) {
+			actual, err := Render([]byte(input))
+			if err != nil {
+				t.Fatal(err)
+			}
 
-		actual, err := Render([]byte(input))
-		if err != nil {
-			t.Fatal(err)
-		}
-
-		err = snapshot.WithLabel(name).Matches(actual.String())
-		// returns an error with a pretty diff if the snapshot doesn't match
-		if err != nil {
-			// print the label and the diff
-			t.Fatalf("Snapshot didn't match.\n - '%s'\n%s",
-				name,
-				err.Error())
-		}
+			cupaloy.SnapshotT(t, actual.String())
+		})
 	}
 }
